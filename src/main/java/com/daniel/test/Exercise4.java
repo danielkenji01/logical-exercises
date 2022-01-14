8476ef4d390b2815c3b431839985aa644ec92c92
danielkenji01 <daniel_kenji_1@hotmail.com>
2022-01-14 10:47:25 -0300

    melhoria no exercício 4

--- src/main/java/com/daniel/test/Exercise4.java
+++ src/main/java/com/daniel/test/Exercise4.java
@@ -5,54 +5,53 @@ package com.daniel.test;
  *  In case of a tie, both teams are awarded with 1 point each.
  *  Given int[] wins and int[] ties, and knowing that the i'th element of wins and ties correspond to the number of wins and ties respectively for team i;
  *  return the number of points the champion team achieved in this league.
- *  For inputs wins = {1,0,3} and ties = {2, 2, 0}, the result would be 9 (the team at i=2 would win this championship).
+ *  For inputs wins = {1, 0, 3} and ties = {2, 2, 0}, the result would be 9 (the team at i=2 would win this championship).
  */
 public class Exercise4 {
 
     public static void main(String[] args) {
-        int[] wins = new int[]{1, 2, 1, 2};
+        int[] wins = new int[]{1, 2, 1, 3};
         int[] ties = new int[]{2, 2, 4, 1};
 
-        System.out.println(definirCampeao(wins, ties));
+        System.out.println(defineChampion(wins, ties));
     }
 
-    private static String definirCampeao(int[] wins, int[] ties) {
-        int[] totalPoints = calcularPontosVitoria(wins);
+    private static String defineChampion(int[] wins, int[] ties) {
+        int[] totalPoints = initializeArrayPoints(wins.length);
 
-        totalPoints = calcularPontosEmpate(ties, totalPoints);
+        calculatePoints(wins, totalPoints, 3);
+        calculatePoints(ties, totalPoints, 1);
 
-        int indexCampeao = -1;
-        int pontosCampeao = -1;
+        int indexChampion = -1;
+        int pointsChampion = -1;
 
         for (int i = 0; i < totalPoints.length; i++) {
-            if (indexCampeao == -1) {
-                indexCampeao = i;
-                pontosCampeao = totalPoints[i];
+            if (indexChampion == -1) {
+                indexChampion = i;
+                pointsChampion = totalPoints[i];
                 continue;
             }
 
-            if (totalPoints[i] > pontosCampeao) {
-                indexCampeao = i;
-                pontosCampeao = totalPoints[i];
+            if (totalPoints[i] > pointsChampion) {
+                indexChampion = i;
+                pointsChampion = totalPoints[i];
             }
         }
 
-        return "The champion is the team " + indexCampeao + " with " + pontosCampeao + " points.";
+        return "The champion is the team " + indexChampion + " with " + pointsChampion + " points.";
     }
 
-    private static int[] calcularPontosVitoria(int[] wins) {
-        int[] totalPoints = new int[wins.length];
-
-        for (int i = 0; i < wins.length; i++) {
-            totalPoints[i] = wins[i] * 3;
+    private static void calculatePoints(int[] results, int[] totalPoints, int points) {
+        for (int i = 0; i < results.length; i++) {
+            totalPoints[i] += results[i] * points;
         }
-
-        return totalPoints;
     }
 
-    private static int[] calcularPontosEmpate(int[] ties, int[] totalPoints) {
-        for (int i = 0; i < ties.length; i++) {
-            totalPoints[i] += ties[i];
+    private static int[] initializeArrayPoints(int size) {
+        int[] totalPoints = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            totalPoints[i] = 0;
         }
 
         return totalPoints;
